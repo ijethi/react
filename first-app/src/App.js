4eeb1c5e5ee8782dc0b7aa9cafc2cbe2de2cc1b1
jethi <is649926@dal.ca>
2024-03-17 15:00:01 -0300

    portfolio created

---
+++ first-app/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Welcome, I'm Ishant Jethi!</h1>
+        <h2>Aspiring Full Stack Developer</h2>
+      </header>
+
+      <section className="about">
+        <p>
+          Passionate about crafting user-friendly websites and applications, I
+          specialize in both front-end and back-end technologies, including
+          HTML, CSS, JavaScript, React, and Node.js. With a keen eye for design
+          and a strong technical foundation, I stay at the forefront of web
+          development trends and best practices. I'm eager to contribute my
+          skills to a team that values creativity and innovation.
+        </p>
+      </section>
+
+      <section className="links">
+        <h3>Connect with me:</h3>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/ishant-jethi/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/ijethi/JS">GitHub</a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default App;
